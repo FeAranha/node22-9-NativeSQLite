8f37c63584d7c1555527a3be9bd9067771d7a475
FeAranha <felipearanha.c@gmail.com>
2024-10-09 15:41:38 -0300

    API nodeJS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import { createServer } from "node:http"
+import { setTimeout } from "node:timers/promises"
 import { select } from "./db.js"
 
-// select()
+createServer((request, response) => {
+  response.end('{}')
+})
+  .listen(3000, () => console.log('server running at 3000'))
+
+await setTimeout(500)
+
+const result = await (await fetch('http://localhost:3000')).json()
+console.log('GET', result)  
