4dbe3b092f382e6108bc2adf80647ed991854b3c
FeAranha <felipearanha.c@gmail.com>
2024-10-09 12:03:29 -0300

    Items iniciais do DB

---
+++ src/db.js
@@ -0,0 +1,20 @@
+import { DatabaseSync } from "node:sqlite"
+
+function runSeed(items) {
+  
+}
+
+runSeed([
+  {
+    name: 'John Joe1',
+    phone: '123456789',
+  },
+  {
+    name: 'John Joe2',
+    phone: '223456789',
+  },
+  {
+    name: 'John Joe3',
+    phone: '323456789',
+  },
+])
